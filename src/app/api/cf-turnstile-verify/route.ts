1c78d9a95d988877e0c55728ee6b23b744e8bc09
Stefan Wintergerst <wintergerst.stefan@googlemail.com>
2025-08-27 23:13:40 +0200

    validate turnstile serverside

---
+++ src/app/api/cf-turnstile-verify/route.ts
@@ -0,0 +1,84 @@
+import {z} from 'zod';
+
+const inputSchema = z.object({token: z.string()})
+
+const cfResponseSchema = z.object({
+    action: z.string().optional(),
+    cdata: z.string().optional(),
+    challenge_ts: z.string().optional(),
+    hostname: z.string().optional(),
+    messages: z.array(z.string()).optional(),
+    success: z.boolean(),
+    // metadata: z.object({interactive: z.boolean()}).optional(),
+    "error-codes": z.array(
+        z.enum([
+            'missing-input-secret',
+            'invalid-input-secret',
+            'missing-input-response',
+            'invalid-input-response',
+            'invalid-widget-id',
+            'invalid-parsed-secret',
+            'bad-request',
+            'timeout-or-duplicate',
+            'internal-error'
+        ])
+    )
+})
+
+export async function POST(request: Request) {
+
+    const {
+        CF_TURNSTILE_VERIFY_ENDPOINT = 'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+        TURNSTILE_SECRET_KEY
+    } = process.env;
+
+    if (!TURNSTILE_SECRET_KEY) {
+        return new Response('turnstile secret key not set', {
+            status: 500
+        });
+    }
+
+    const input = await request.json();
+
+    const {error, data: parsedInputData} = inputSchema.safeParse(input);
+
+    if (error) {
+        return new Response(JSON.stringify(error), {
+            status: 400,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    }
+
+
+    const {token} = parsedInputData;
+
+    const res = await fetch(CF_TURNSTILE_VERIFY_ENDPOINT, {
+        method: 'POST',
+        body: `secret=${encodeURIComponent(TURNSTILE_SECRET_KEY)}&response=${encodeURIComponent(token)}`,
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    })
+
+    const responseData = await res.json();
+    const {
+        error: cfError,
+        data: parsedResponseData
+    } = cfResponseSchema.safeParse(responseData);
+
+    if (cfError) {
+        console.error(cfError);
+        return new Response('turnstile verification yielded invalid response', {
+            status: 500
+        });
+    }
+
+    return new Response(JSON.stringify(parsedResponseData), {
+        status: parsedResponseData.success ? 200 : 400,
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+}
--- src/app/suggest/page.tsx
+++ src/app/suggest/page.tsx
@@ -7,13 +7,42 @@ import {useSearchParams} from "next/navigation";
 import NextLink from "next/link";
 import {Turnstile} from '@marsidev/react-turnstile'
 import {Suspense, useState} from "react";
+import {z} from 'zod';
 
+const verifySchema = z.object({
+    success: z.boolean(),
+    'error-codes': z.array(z.string()).optional(),
+});
 
 function Suggest() {
     const query = useSearchParams();
     const result = query.get('result');
 
+    const [error, setError] = useState<string | null>(null);
     const [isVerified, setIsVerified] = useState<boolean>(false);
+
+    const verifyTurnstile = async (token: string) => {
+        const res = await fetch('/api/cf-turnstile-verify', {
+            method: 'POST',
+            body: JSON.stringify({token}),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+
+        const data = await res.json();
+
+        const {
+            data: parsedData,
+        } = verifySchema.safeParse(data);
+
+        if (parsedData?.success) {
+            setIsVerified(true);
+        } else {
+            setError(parsedData?.["error-codes"]?.join(' ') ?? 'unknown error');
+        }
+
+    }
 
     if (result) {
         return (
@@ -27,6 +56,7 @@ function Suggest() {
         )
     }
 
+
     const turnStileKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY;
 
     if (!turnStileKey) {
@@ -39,14 +69,22 @@ function Suggest() {
                 <h1>estimation corgi</h1>
             </NextLink>
             <form className={styles.form} action={suggestMessage}>
-                <input className={styles.input} type="text" name="message" placeholder='message (max 72 characters)' required/>
+                <input className={styles.input} type="text" name="message" placeholder='message (max 72 characters)'
+                       required/>
                 <input className={styles.input} type="text" name="suggestedBy" placeholder='your name (optional)'/>
                 {
                     turnStileKey && (
                         <Turnstile
                             siteKey={turnStileKey}
-                            onSuccess={() => setIsVerified(true)}
+                            onSuccess={(token) => {
+                                void verifyTurnstile(token);
+                            }}
                         />
+                    )
+                }
+                {
+                    error && (
+                        <p className={styles.error}>error validating turnstile: {error}</p>
                     )
                 }
                 {
