a7da9c874bdb71542f0ebea72717d4728f14df9f
Stefan Wintergerst <wintergerst.stefan@googlemail.com>
2025-12-31 03:44:43 +0100

    Remove experimental Turbopack filesystem cache configuration from `next.config.ts`
    - Clean up unused experimental settings and comments related to cloudflare workers builds.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from 'next';
 
-// TODO: does not work well with cloudflare workers builds
-// TODO: since build env vars need to be set manually and cannot be synced with normal env vars
-// import "@/env";
-
-const nextConfig: NextConfig = {
-	experimental: {
-		turbopackFileSystemCacheForDev: true,
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
